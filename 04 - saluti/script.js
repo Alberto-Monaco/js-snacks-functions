e2f652651ae136e1990029ca7125e337f62ddc2e
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-07 15:06:40 +0200

    complete 4 exercise

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,16 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = 'Mario'
 
 // Dichiara la funzione qui.
-
+function saluto(name) {
+	let saluto = []
+	saluto[1] = name
+	saluto[0] = 'Ciao'
+	return `${saluto[0]} ${saluto[1]}`
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(saluto(name))
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
