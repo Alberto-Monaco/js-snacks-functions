d122c26ba80dec1f8ea9053136840a88c508f5c6
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-07 14:25:13 +0200

    complete first exercise

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,16 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
-const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
-
+const names = ['Anna', 'Luca', 'Marco', 'Adele', 'Giovanni', 'Alessandra']
+let initial_name = []
 // Dichiara la funzione qui.
-
+function initial_letter(names) {
+	for (let i = 0; i < names.length; i++) {
+		initial_name[i] = names[i].charAt(0)
+	}
+	return initial_name
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(initial_letter(names))
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
