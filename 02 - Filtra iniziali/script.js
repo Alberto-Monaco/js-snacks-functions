7c742b00b3ce97aa0df04abb9b39e8277f19e5e9
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-07 14:38:41 +0200

    complete 2 exercise

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,13 +1,22 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
-const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const names = ['Anna', 'Luca', 'Marco', 'Adele', 'Laura', 'Alessandra']
+let initial_name = []
 
 // Dichiara la funzione qui.
-
+function initial_letter(letter, names) {
+	for (let i = 0; i < names.length; i++) {
+		if (letter == names[i].charAt(0)) {
+			initial_name[i] = names[i]
+		} else {
+			initial_name[i] = ''
+		}
+	}
+	return initial_name
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(initial_letter('A', names))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
